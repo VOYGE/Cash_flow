2791e64b70ecdbbf792f4d9ea943b0aaebf0c063
Danila Krepishkov <daniil.krepishkov@gmail.com>
2026-06-29 13:44:21 +0300

    refactor: Refactor form adding

--- cashflow/urls.py
+++ cashflow/urls.py
@@ -1,7 +1,24 @@
 from django.urls import path
-from .views import CashFlowCreateView, CashFlowListView
+from .views import (
+    CashFlowListView,
+    CashFlowCreateView,
+    load_categories,
+    load_subcategories,
+)
 
 urlpatterns = [
     path("", CashFlowListView.as_view(), name="cashflow_list"),
     path("create/", CashFlowCreateView.as_view(), name="cashflow_create"),
+
+    path(
+        "ajax/load-categories/",
+        load_categories,
+        name="ajax_load_categories",
+    ),
+
+    path(
+        "ajax/load-subcategories/",
+        load_subcategories,
+        name="ajax_load_subcategories",
+    ),
 ]
--- cashflow/views.py
+++ cashflow/views.py
@@ -3,6 +3,8 @@ from .models import CashFlow
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from .forms import CashFlowForm
+from django.http import JsonResponse
+from .models import CashFlow, Category, SubCategory
 
 
 class CashFlowListView(ListView):
@@ -15,3 +17,22 @@ class CashFlowCreateView(CreateView):
     form_class = CashFlowForm
     template_name = "cashflow/cashflow_form.html"
     success_url = reverse_lazy("cashflow_list")
+
+def load_categories(request):
+    operation_type_id = request.GET.get("operation_type")
+
+    categories = Category.objects.filter(
+        operation_type_id=operation_type_id
+    ).values("id", "name")
+
+    return JsonResponse(list(categories), safe=False)
+
+
+def load_subcategories(request):
+    category_id = request.GET.get("category")
+
+    subcategories = SubCategory.objects.filter(
+        category_id=category_id
+    ).values("id", "name")
+
+    return JsonResponse(list(subcategories), safe=False)
